1658dd100f4462d852601d06da665115cecad8c4
Jhon Eduard <eduardcamayo13@gmail.com>
2024-04-07 17:45:22 -0500

    Quitando el metodo post de la ruta users

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -10,13 +10,5 @@ export const GET = async () => {
     return NextResponse.json(users);
 };
 
-export const POST = async (req: any) => {
-    await connectDB();
-    const data = await req.json();
-    const users = await registerNewUser(data.datos);
-    if(users === "ALREADY_USER"){
-      return new Response("ALREADY_USER", {status: 401})
-    }
 
-    return new Response("OK", {status: 200})
-};
+
